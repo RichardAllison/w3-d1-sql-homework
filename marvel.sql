ce75a0d33e1e2dbb0f82587ed3e96cb2485906b1
Richard Allison <richard.allison.18@outlook.com>
2017-12-04 16:55:04 +0000

    completed questions 3 to 6

--- marvel.sql
+++ marvel.sql
@@ -147,7 +147,7 @@ INSERT INTO attendances (person_id, movie_id) VALUES (3, 4);
 -- ## Questions
 --
 -- 1. Return ALL the data in the 'movies' table.
--- SELECT * FROM movies;
+SELECT * FROM movies;
 -- id |                title                | year | show_time
 -- ----+-------------------------------------+------+-----------
 --  1 | Iron Man                            | 2008 | 17:30
@@ -169,7 +169,7 @@ INSERT INTO attendances (person_id, movie_id) VALUES (3, 4);
 -- (16 rows)
 
 -- 2. Return ONLY the name column from the 'people' table
--- SELECT name FROM people;
+SELECT name FROM people;
 -- name
 -- -------------------
 -- Dave Ellis
@@ -196,18 +196,94 @@ INSERT INTO attendances (person_id, movie_id) VALUES (3, 4);
 -- (21 rows)
 
 -- 3. Oops! Someone at CodeClan spelled Lorna's name wrong! Change it to reflect the proper spelling (Noble)
--- UPDATE people SET name = 'Lorna Noble' WHERE name = 'Lorna Nobel-Prize';
+UPDATE people SET name = 'Lorna Noble' WHERE name = 'Lorna Nobel-Prize';
 -- UPDATE 1
 -- SELECT name FROM people;
 -- name
 -- ------------------
--- ...
+-- Dave Ellis
+-- Joe Ryan
+-- Richard Allison
+-- Alison Wood
+-- Olga Maunsell
+-- Graeme Brown
+-- Eric Carles
+-- David Rawson
+-- Ross Hill
+-- Mark Blanford
+-- Douglas Sangster
+-- Giuseppe Ibba
+-- Kris Anderson
+-- Vishal Sharma
+-- Manny Chita
+-- Amy Morrison
+-- Joe Plevin
+-- Robert Henderson
+-- Max Robson
+-- Alex Bazlinton
 -- Lorna Noble
 -- (21 rows)
 
 -- 4. Return ONLY your name from the 'people' table.
+SELECT name FROM people WHERE name = 'Richard Allison';
+-- name
+-- -----------------
+--  Richard Allison
+-- (1 row)
+
 -- 5. The cinema is showing 'Batman Begins', but Batman is DC, not Marvel! Delete the entry from the 'movies' table.
+DELETE FROM movies WHERE id = 9;
+-- DELETE 1
+-- SELECT * FROM movies;
+-- id |                title                | year | show_time
+-- ----+-------------------------------------+------+-----------
+--   1 | Iron Man                            | 2008 | 17:30
+--   2 | The Incredible Hulk                 | 2008 | 17:10
+--   3 | Iron Man 2                          | 2010 | 20:05
+--   4 | Thor                                | 2011 | 21:05
+--   5 | Captain America: The First Avenger  | 2011 | 12:05
+--   6 | Avengers Assemble                   | 2012 | 13:05
+--   7 | Iron Man 3                          | 2013 | 22:00
+--   8 | Thor: The Dark World                | 2013 | 19:30
+--  10 | Captain America: The Winter Soldier | 2014 | 20:15
+--  11 | Guardians of the Galaxy             | 2014 | 17:55
+--  12 | Avengers: Age of Ultron             | 2015 | 21:10
+--  13 | Ant-Man                             | 2015 | 20:40
+--  14 | Captain America: Civil War          | 2016 | 13:50
+--  15 | Doctor Strange                      | 2016 | 21:30
+--  16 | Guardians of the Galaxy 2           | 2017 | 15:50
+-- (15 rows)
+
 -- 6. Create a new entry in the 'people' table with the name of one of the instructors
+INSERT INTO people(name) VALUES ('John Harper');
+-- INSERT 0 1
+-- SELECT * FROM people;
+-- id |       name
+-- ----+-------------------
+-- 1 | Dave Ellis
+-- 2 | Joe Ryan
+-- 3 | Richard Allison
+-- 4 | Alison Wood
+-- 5 | Olga Maunsell
+-- 6 | Graeme Brown
+-- 7 | Eric Carles
+-- 8 | David Rawson
+-- 9 | Ross Hill
+-- 10 | Mark Blanford
+-- 11 | Douglas Sangster
+-- 12 | Giuseppe Ibba
+-- 13 | Kris Anderson
+-- 14 | Vishal Sharma
+-- 15 | Manny Chita
+-- 16 | Amy Morrison
+-- 17 | Joe Plevin
+-- 18 | Robert Henderson
+-- 19 | Max Robson
+-- 20 | Lorna Noble
+-- 21 | Alex Bazlinton
+-- 22 | John Harper
+-- (22 rows)
+
 -- 7. Alex has decided to hijack our movie evening, Remove him from the table of people.
 -- 8. The cinema has just heard that they will be holding an exclusive midnight showing of 'Guardians of the Galaxy 2'!! Create a new entry in the 'movies' table to reflect this.
 -- 9. The cinema would also like to make the Guardian movies a back to back feature. Update the 'Guardians of the Galaxy' show time from 17:55 to 21:30
